8f1c113c9bb865bc3952059315d56aca6b012189
walidhassan1011 <walidhassan009@gmail.com>
2022-12-25 23:56:59 +0200

    start-build-DB

--- swr_project/Form1.cs
+++ swr_project/Form1.cs
@@ -1,3 +1,9 @@
+
+using swr_project.controllers;
+using swr_project.Models;
+using System;
+using System.Windows.Forms;
+
 namespace swr_project
 {
     public partial class Form1 : Form
@@ -9,6 +15,14 @@ namespace swr_project
 
         private void Form1_Load(object sender, EventArgs e)
         {
+
+        }
+
+        private  void button1_Click(object sender, EventArgs e)
+        {
+            DB_helpers db = new DB_helpers();
+            
+            db.addNewInvoice(new Invoice { invoiceNumber = "123" });
 
         }
     }
